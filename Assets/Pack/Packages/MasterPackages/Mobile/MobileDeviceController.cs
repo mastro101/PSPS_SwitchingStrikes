655d6c5cd211b9798a9832462327a5110e178846
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-11-25 17:23:19 +0100

    Vertical screen

---
+++ Assets/Pack/Packages/MasterPackages/Mobile/MobileDeviceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileDeviceController : MonoBehaviour
+{
+    public static void RotateVerticalScreen()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+    
+    public static void RotateHorizontalScreen()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+}
