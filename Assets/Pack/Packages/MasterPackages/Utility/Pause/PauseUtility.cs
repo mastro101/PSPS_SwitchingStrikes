19091cb033151a2abd33134e4fd84001cd5b9030
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-12-21 16:13:26 +0100

    Pause menu implementation

---
+++ Assets/Pack/Packages/MasterPackages/Utility/Pause/PauseUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUtility : MonoBehaviour
+{
+    static bool pause = false;
+    static float timeScalePrePause = 1f;
+
+    public static void Pause()
+    {
+        if (!pause)
+        {
+            pause = true;
+            timeScalePrePause = Time.timeScale;
+            Time.timeScale = 0;
+        }
+    }
+
+    public static void Resume()
+    {
+        if (pause)
+        {
+            pause = false;
+            Time.timeScale = timeScalePrePause;
+        }
+    }
+
+    public static void PauseAndResume()
+    {
+        if (pause)
+        {
+            Resume();
+        }
+        else
+            Pause();
+    }
+}
