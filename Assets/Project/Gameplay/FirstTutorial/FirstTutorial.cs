de88ccd715016ffcacce86eca0e3d920317a35c6
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2021-02-24 12:02:51 +0100

    first time you play show the tutorial

---
+++ Assets/Project/Gameplay/FirstTutorial/FirstTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTutorial : MonoBehaviour
+{
+    [SerializeField] GameObject tutorialPanel;
+    bool firstGameplay;
+
+    private void Awake()
+    {
+        firstGameplay = System.Convert.ToBoolean(PlayerPrefs.GetInt("firstGameplay", System.Convert.ToInt32(true)));
+
+        if (firstGameplay == true)
+        {
+            tutorialPanel.SetActive(true);
+            PauseUtility.Pause();
+            PlayerPrefs.SetInt("firstGameplay", System.Convert.ToInt32(false));
+        }
+    }
+}
